33b6eb20dad1b1f47e896992568d51c3fccd6855
Juls-1979 <yulia.ushakova@gmail.com>
2022-11-02 07:20:41 +0000

    first draft version of the entertainment app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [advice, setAdvice] = useState("");
+
+  const getAdvice = (e) => {
+    e.prevent.default(e);
+  getAdvice(setAdvice);
+}
+
+  useEffect(() => {
+    const getAdvice = async() => {
+    const response = await fetch(`http://www.boredapi.com/api/activity/`);
+    const data = await response.json();
+    setAdvice(data.activity)
+    }
+    getAdvice();
+  }, [])
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <h2> Are you bored?</h2>
+        <h2>Hit this button for ideas</h2>
+        </div>
+
+        <div className="container">
+          <button onClick={getAdvice}><img src="https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/000000/external-idea-achievements-flaticons-lineal-color-flat-icons.png" alt="lihgtbulb"/></button>
+        </div>
+        <div className="container">
+        <p>{setAdvice}</p>
+        </div>
+      </div>
   );
 }
 
